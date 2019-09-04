3a69911f042704125d4dffef2c74d29ce6e7dcb8
MrOnimus <36086974+MrOnimus@users.noreply.github.com>
2019-09-04 17:35:43 +0300

    0.1.1
    Trying to build normal recursion

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: rdremora <rdremora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/03 03:23:57 by xenomorph         #+#    #+#             */
-/*   Updated: 2019/09/04 15:33:30 by rdremora         ###   ########.fr       */
+/*   Updated: 2019/09/04 17:17:18 by rdremora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,27 @@ typedef struct		s_map
 
 void				ft_optimtetr(char **m_chunk);
 void				ft_minimizetetr(char **chunk);
-void				ft_clearmap(char **map);
-char				**ft_initmap(char **map, size_t size);
-void				ft_printmap(char **map);
 
-//t_map				*ft_initmap(t_map *map, size_t size);
-//void				ft_printmap(char **chunk);
-//void				ft_clearmap(t_map *map);
+//void				ft_clearmap(char **map);
+//char				**ft_initmap(char **map, size_t size);
+//void				ft_printmap(char **map);
 
-char				**ft_inserttetrotomap(char **map, size_t map_size,
-t_tetr *tet, size_t x_m, size_t y_m);
+t_map				*ft_initmap(t_map *map, size_t size);
+void				ft_printmap(t_map *map);
+void				ft_clearmap(t_map *map);
+
+//char				**ft_inserttetros(char **map, size_t map_size,
+//t_tetr *tet, size_t x_m, size_t y_m);
+t_map				*ft_inserttetros(t_map *map, t_tetr *tet,
+size_t x_m, size_t y_m);
 char				*ft_mapstrnew(size_t size);
 void				ft_bruteforce(t_tetr *head);
 size_t				ft_findmapsize(size_t tetroscount);
 size_t				ft_gettetrmaxdmnsn(char *chunk, size_t i);
 size_t				ft_lstlen(t_tetr *head);
+t_map				*ft_placetetr(t_map *map, t_tetr *tet,
+size_t x_m, size_t y_m);
+size_t				ft_checkplacement(t_map *map, t_tetr *tet,
+size_t x_m, size_t y_m);
 
 #endif
--- map_operations.c
+++ map_operations.c
@@ -6,7 +6,7 @@
 /*   By: rdremora <rdremora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 15:40:44 by rdremora          #+#    #+#             */
-/*   Updated: 2019/09/04 15:33:15 by rdremora         ###   ########.fr       */
+/*   Updated: 2019/09/04 17:30:24 by rdremora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 или проверку возможности вставки */
 /* TODO: Validate tetromino by predefined examples */
 
-char	**ft_inserttetrotomap(char **map, size_t map_size, t_tetr *tet,
+/*char	**ft_inserttetros(char **map, size_t map_size, t_tetr *tet,
 size_t x_m, size_t y_m)
 {
 	size_t	i;
@@ -37,24 +37,75 @@ size_t x_m, size_t y_m)
 	}
 	if (!tet->chunk[i])
 		if ((x_m == map_size && y_m == map_size) || !tet->chunk[i])
-			return (ft_inserttetrotomap(map, map_size, tet->next, 0, 0));
+			return (ft_inserttetros(map, map_size, tet->next, 0, 0));
 		else if (x_m == map_size)
-			return (ft_inserttetrotomap(map, map_size, tet, 0, ++y_m));
+			return (ft_inserttetros(map, map_size, tet, 0, ++y_m));
 		else
-			return (ft_inserttetrotomap(map, map_size, tet, ++x_m, y_m));
+			return (ft_inserttetros(map, map_size, tet, ++x_m, y_m));
 	else
 		return (NULL);
+}*/
+
+size_t	ft_checkplacement(t_map *map, t_tetr *tet, size_t x_m, size_t y_m)
+{
+	size_t	i;
+
+	i = 0;
+	while (tet->chunk[i])
+	{
+		if ((size_t)tet->chunk[i] - (size_t)'0' < map->size &&
+		(size_t)tet->chunk[i + 1] - (size_t)'0' < map->size)
+			if (map->chunk[tet->chunk[i] - '0'][tet->chunk[i + 1] - '0'] != '.')
+				return (NULL);
+		i += 2;
+	}
+	if (tet->chunk[i])
+		return (NULL);
+	return (1);
+}
+
+t_map	*ft_placetetr(t_map *map, t_tetr *tet, size_t x_m, size_t y_m)
+{
+
+}
+
+t_map	*ft_inserttetros(t_map *map, t_tetr *tet, size_t x_m, size_t y_m)
+{
+	size_t	i;
+
+	if (!map || map->size < 2)
+		return (NULL);
+	if (!tet)
+		return (map);
+	if (ft_checkplacement(map, tet, x_m, y_m))
+	{
+		map = ft_placetetr(map, tet, x_m, y_m);
+	}
+	else
+		return (NULL);
+	/*while (tet->chunk[(i += 2)])
+	{
+		if ((size_t)tet->chunk[i] - (size_t)'0' < map->size &&
+		(size_t)tet->chunk[i + 1] - (size_t)'0' < map->size)
+			if (map->chunk[tet->chunk[i] - '0'][tet->chunk[i + 1] - '0'] == '.')
+				map->chunk[tet->chunk[i] - '0'][tet->chunk[i + 1] - '0'] =
+				tet->letter;
+	}*/
+	if ((x_m == map->size && y_m == map->size))
+		return (ft_inserttetros(map, tet->next, 0, 0));
+	else if (x_m == map->size)
+		return (ft_inserttetros(map, tet, 0, ++y_m));
+	else
+		return (ft_inserttetros(map, tet, ++x_m, y_m));
 }
 
 void	ft_bruteforce(t_tetr *head)
 {
-	size_t		map_size;
-	char		**map;
+	t_map		*map;
 
-	map_size = ft_findmapsize(ft_lstlen(head));
-	map = NULL;
-	while (!ft_inserttetrotomap(map, ++map_size, head, 0, 0))
-		map = ft_initmap(map, map_size);
+	map = ft_initmap(NULL, ft_findmapsize(ft_lstlen(head)));
+	while (!ft_inserttetros(map, head, 0, 0))
+		map = ft_initmap(map, ++map->size);
 	ft_printmap(map);
 	ft_clearmap(map);
 }
